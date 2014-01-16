02512224bcfe6be9ab0f6204c4b0db47672b2901
pyrosama <pyrosama@comcast.net>
2014-01-16 17:04:48 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AAAWebsite::Application.config.secret_key_base = 'f0f921cd1bf16e01b8ec66a23bfe1cef48da4eee375933d0c214db7a407b345f9dad6196c89e34b548048d8c1f0666dc6d94ce71a3611969f10a7c01d8830222'
